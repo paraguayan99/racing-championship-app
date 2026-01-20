f16e825dd174c8e9e0f8f23b27fb4207dad768b1
paraguayan99 <paraguayan99@laposte.net>
2026-01-20 16:11:07 +0100

    Modif .htaccess pour rendre inaccessible côté front les dossiers img flags teams

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -7,48 +7,6 @@ use App\Models\UsersModel;
 
 class AuthController extends Controller
 {
-    // Fonctionne pour WAMPSERVER
-    // Traitement de la connexion
-    // public function login()
-    // {
-    //     $error = '';
-
-    //     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    //         $email = $_POST['email'] ?? '';
-    //         $password = $_POST['password'] ?? '';
-    //         $csrf_token = $_POST['csrf_token'] ?? '';
-
-    //         // Vérification CSRF
-    //         if (!Auth::validateCSRF($csrf_token)) {
-    //             $error = "Token CSRF invalide !";
-    //         } else {
-    //             $user = UsersModel::findByEmail($email);
-
-    //             if ($user && password_verify($password, $user->password_hash)) {
-    //                 // Connexion sécurisée
-    //                 session_start();
-
-    //                 // Anti session fixation
-    //                 session_regenerate_id(true);
-    //                 // Regeneration du token CSRF après connexion
-    //                 $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-
-    //                 $_SESSION['user_id'] = $user->id;
-    //                 $_SESSION['role_id'] = $user->role_id;
-    //                 $_SESSION['role'] = UsersModel::getRoleName($user->role_id);
-
-    //                 header('Location: index.php?controller=dashboard&action=index');
-    //                 exit();
-    //             } else {
-    //                 $error = "Identifiants invalides";
-    //             }
-    //         }
-    //     }
-
-    //     // Affiche le formulaire avec l’erreur éventuelle
-    //     $this->render('auth/login', ['error' => $error]);
-    // }
-
     // Fonctionne pour OVH
     // Traitement de la connexion
     public function login()
